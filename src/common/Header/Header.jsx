20e74902b73aa6e714751fe0a4f8c1ed5707d505
Dave86dev <davidochando1986@gmail.com>
2024-04-03 11:12:36 +0200

    feat: plantilla básica con Redux instalado en la aplicación

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,8 +1,20 @@
 import './Header.css'
+import { CLink } from "../CLink/CLink"
 export const Header = () => {
      return (
          <div className="header-design">
-
+            <CLink 
+                path="/"
+                title="Home"
+            />
+            <CLink 
+                path="/login"
+                title="Login"
+            />
+            <CLink 
+                path="/register"
+                title="Register"
+            />
          </div>
      )
 }
